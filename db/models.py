283eeebcf9a482f77b2460b1fa8312b2fe7895c2
Yevhen Nastoiashchyi <Nastoyashiy@i.ua>
2025-06-14 19:15:10 +0300

    solution

--- db/models.py
+++ db/models.py
@@ -67,7 +67,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
 
 
 class Ticket(models.Model):
@@ -90,7 +90,7 @@ class Ticket(models.Model):
     def __str__(self) -> str:
         return (
             f"{self.movie_session.movie.title} "
-            f"{self.movie_session.show_time.strftime('%Y-%m-%d %H:%M:%S')} "
+            f"{self.movie_session.show_time.strftime("%Y-%m-%d %H:%M:%S")} "
             f"(row: {self.row}, seat: {self.seat})"
         )
 
